d96f7a3be4899d919094405a84bc153aeea67f41
DeWickz <44259504+DeWickz@users.noreply.github.com>
2019-11-24 00:19:24 +0700

    Create thread and add jobs
    in main

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.util.concurrent.*;
 public class Main{
+    private static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(15);
     public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while (true) {
+                try {
+                    queue.take().run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+    
+            }
+        });
+        t.start();
+        queue.add(() -> {
+            Job j1 = new Job();
+        });
+        queue.add(() -> {
+            Job j2 = new Job();
+        });
+        queue.add(() -> {
+            Job j3 = new Job();
+        });
+
+
         GanttChart g = new GanttChart();
         g.display();
      }
