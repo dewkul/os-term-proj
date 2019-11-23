0f6be96f74b78cc6e1c33ac1e3030e50b749304e
dewkul <jirapat0811@gmail.com>
2019-11-24 01:24:23 +0700

    Prepare for Gantt chart

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args) {
         GanttChart g = new GanttChart();
+
+        
+        List<Integer> proc_id = Arrays.asList(0, 1, 2, 0, 1, 2);
+        List<Integer> exec_time = Arrays.asList(3, 3, 3, 2, 3, 1);
+        g.addAllProc(proc_id, exec_time);
         g.display();
      }
 }
