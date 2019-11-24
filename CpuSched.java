ebd6afed5260ce44c6612a2b44f62606f4eb0aef
pollakit <60090031@kmitl.ac.th>
2019-11-24 22:40:38 +0700

    Update CpuSched.java

--- CpuSched.java
+++ CpuSched.java
@@ -69,6 +69,7 @@ public class CPUScheduler
     int timeQuantum;
     int currentTime = 0;
     int totalTime = 0;
+    int currentTimeSlot = 0;
     int pIndex = 0;
 
     public CPUScheduler(ArrayList<Process> readyQueue, int timeQuantum) {
@@ -81,12 +82,19 @@ public class CPUScheduler
 
         while(!readyQueue.isEmpty()){
             this.readyQueue.get(pIndex).setRemainBurstTime(this.readyQueue.get(pIndex).getRemainBurstTime()-1);
+            System.out.println("Current Time: "+ this.currentTime + " CPU is running processID:" + this.readyQueue.get(pIndex).getProcessID());
             if(this.readyQueue.get(pIndex).getRemainBurstTime()==0){
-                
+                this.currentTimeSlot = 0;
+                this.readyQueue.get(pIndex).setTurnAroundTime(this.currentTime);
+                this.readyQueue.get(pIndex).setWaitingTime(this.readyQueue.get(pIndex).getTurnAroundTime()-this.readyQueue.get(pIndex).getBurstTime());
                 System.out.println(this.readyQueue.get(pIndex).toString());
             }
+            this.currentTimeSlot++;
+            if(this.currentTimeSlot >= this.timeQuantum){
+                this.currentTimeSlot = 0;
+                pIndex = (pIndex+1)%this.readyQueue.size();
+            }
 
-            pIndex = (pIndex+1)%this.readyQueue.size();
             this.currentTime++;
         }
     }
