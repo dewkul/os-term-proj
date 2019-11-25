3c7cb171fc6112eea01f98cbb03ba29e81f23d02
pollakit <60090031@kmitl.ac.th>
2019-11-25 23:45:59 +0700

    Update CPUScheduler.java

--- CPUScheduler.java
+++ CPUScheduler.java
@@ -10,11 +10,10 @@ public class CPUScheduler implements Runnable {
     int totalBurstTime = 0;
     int totalWaitingTime = 0;
     int totalTurnaround = 0;
-    static ArrayList<Job> readyQueue;
-    // static ReadyQueue readyQueue;
+    static ReadyQueue readyQueue;
 
-    public CPUScheduler(int timeQuantum) {
-        CPUScheduler.readyQueue = new ArrayList<Job>();
+    public CPUScheduler(final ReadyQueue readyQueue,final int timeQuantum) {
+        this.readyQueue = readyQueue;
         this.timeQuantum = timeQuantum;
     }
     @Override
@@ -23,8 +22,9 @@ public class CPUScheduler implements Runnable {
         for (final Job x : this.readyQueue) {
             this.totalBurstTime += x.getBurstTime();
         }
-
+        //System.out.println(readyQueue.toString());
         while (!readyQueue.isEmpty()) {
+
             System.out.println("Current Time: " + this.currentTime + " CPU is running processID:"
                     + this.readyQueue.get(pIndex).getProcessID());
             // reduce remain burst time of the process by 1 and add time
@@ -36,7 +36,6 @@ public class CPUScheduler implements Runnable {
             // the process is completed
             if (this.readyQueue.get(pIndex).getRemainBurstTime() == 0) {
                 this.currentTimeSlot = 0;
-                // pIndex = (pIndex + 1) % this.readyQueue.size();
 
                 this.readyQueue.get(pIndex).setTurnAroundTime(currentTime- readyQueue.get(pIndex).getArrivalTime());
                 this.readyQueue.get(pIndex).setWaitingTime(
@@ -55,7 +54,30 @@ public class CPUScheduler implements Runnable {
             // exceed limit of time quantum
             if (this.currentTimeSlot >= this.timeQuantum) {
                 this.currentTimeSlot = 0;
-                pIndex = (pIndex + 1) % this.readyQueue.size();
+
+                //finding the next process to run that aready arrived
+                for(int i = 0; i< this.readyQueue.size(); i++){
+                    pIndex = (pIndex + 1) % this.readyQueue.size();
+
+                    if(readyQueue.get(pIndex).getArrivalTime() <= currentTime){
+                        break;
+                    }
+                }
+
+                //there is no process already arrived add current time until any process arrived
+
+                if(readyQueue.get(pIndex).getArrivalTime() > currentTime){
+                    //find the most almost arrive process
+                    pIndex = 0;
+                    for(int i = 0; i< this.readyQueue.size(); i++){
+                        if(readyQueue.get(i).getArrivalTime() <= readyQueue.get(pIndex).getArrivalTime()){
+                            pIndex = i;
+                        }
+                    }
+                    while( readyQueue.get(pIndex).getArrivalTime() > currentTime){
+                        currentTime++;
+                    }
+                }
             }
         }
 
@@ -64,3 +86,75 @@ public class CPUScheduler implements Runnable {
         System.out.println("Total burst time = " + totalBurstTime);
     }
 }
+
+
+
+
+
+
+// import java.util.ArrayList;
+
+// public class CPUScheduler implements Runnable {
+//     int timeQuantum;
+//     int currentTime = 0;
+//     int currentTimeSlot = 0;
+//     int pIndex = 0;
+
+//     int nProcess;
+//     int totalBurstTime = 0;
+//     int totalWaitingTime = 0;
+//     int totalTurnaround = 0;
+//     static ArrayList<Job> readyQueue;
+//     // static ReadyQueue readyQueue;
+
+//     public CPUScheduler(int timeQuantum) {
+//         CPUScheduler.readyQueue = new ArrayList<Job>();
+//         this.timeQuantum = timeQuantum;
+//     }
+//     @Override
+//     public void run() {
+//         this.nProcess = readyQueue.size();
+//         for (final Job x : this.readyQueue) {
+//             this.totalBurstTime += x.getBurstTime();
+//         }
+
+//         while (!readyQueue.isEmpty()) {
+//             System.out.println("Current Time: " + this.currentTime + " CPU is running processID:"
+//                     + this.readyQueue.get(pIndex).getProcessID());
+//             // reduce remain burst time of the process by 1 and add time
+//             readyQueue.get(pIndex).setRemainBurstTime(readyQueue.get(pIndex).getRemainBurstTime() - 1);
+
+//             currentTime++;
+//             currentTimeSlot++;
+
+//             // the process is completed
+//             if (this.readyQueue.get(pIndex).getRemainBurstTime() == 0) {
+//                 this.currentTimeSlot = 0;
+//                 // pIndex = (pIndex + 1) % this.readyQueue.size();
+
+//                 this.readyQueue.get(pIndex).setTurnAroundTime(currentTime- readyQueue.get(pIndex).getArrivalTime());
+//                 this.readyQueue.get(pIndex).setWaitingTime(
+//                         readyQueue.get(pIndex).getTurnAroundTime() - readyQueue.get(pIndex).getBurstTime());
+//                 System.out.println(readyQueue.get(pIndex).toString());
+
+//                 totalWaitingTime += readyQueue.get(pIndex).getTurnAroundTime() - readyQueue.get(pIndex).getBurstTime();
+//                 totalTurnaround += currentTime;
+
+//                 this.readyQueue.remove(pIndex);
+
+//                 if (this.readyQueue.size() == 1) {
+//                     this.pIndex = 0;
+//                 }
+//             }
+//             // exceed limit of time quantum
+//             if (this.currentTimeSlot >= this.timeQuantum) {
+//                 this.currentTimeSlot = 0;
+//                 pIndex = (pIndex + 1) % this.readyQueue.size();
+//             }
+//         }
+
+//         System.out.println("Average waiting time = " + (float) totalWaitingTime / (float) nProcess);
+//         System.out.println("Average turn around time = " + (float) totalTurnaround / (float) nProcess);
+//         System.out.println("Total burst time = " + totalBurstTime);
+//     }
+// }
