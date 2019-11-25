dcdb2f21c0982632661a38dc5c72850f080841a7
pollakit <60090031@kmitl.ac.th>
2019-11-25 14:42:25 +0700

    Update CPUScheduler.java

--- CPUScheduler.java
+++ CPUScheduler.java
@@ -1,4 +1,4 @@
-public class CPUScheduler {
+public class CPUScheduler implements Runnable{
     ReadyQueue readyQueue;
     int timeQuantum;
     int currentTime = 0;
@@ -13,7 +13,8 @@ public class CPUScheduler {
     public CPUScheduler(final ReadyQueue readyQueue, final int timeQuantum) {
         this.readyQueue = readyQueue;
         this.timeQuantum = timeQuantum;
-
+    }
+    public void run(){
         this.nProcess = readyQueue.size();
         for (final Process x : this.readyQueue) {
             this.totalBurstTime += x.getBurstTime();
@@ -27,24 +28,26 @@ public class CPUScheduler {
 
             currentTime++;
             currentTimeSlot++;
-            // the process is completed
+            
+// the process is completed
             if (this.readyQueue.get(pIndex).getRemainBurstTime() == 0) {
                 this.currentTimeSlot = 0;
                 // pIndex = (pIndex + 1) % this.readyQueue.size();
-                totalWaitingTime += currentTime;
-                totalTurnaround += readyQueue.get(pIndex).getTurnAroundTime() - readyQueue.get(pIndex).getBurstTime();
 
                 this.readyQueue.get(pIndex).setTurnAroundTime(currentTime);
                 this.readyQueue.get(pIndex).setWaitingTime(
                         readyQueue.get(pIndex).getTurnAroundTime() - readyQueue.get(pIndex).getBurstTime());
                 System.out.println(readyQueue.get(pIndex).toString());
+
+                totalWaitingTime += readyQueue.get(pIndex).getTurnAroundTime() - readyQueue.get(pIndex).getBurstTime();
+                totalTurnaround += currentTime;
+
                 this.readyQueue.remove(pIndex);
 
                 if(this.readyQueue.size() == 1){
                     this.pIndex = 0;
                 }
             }
-
             // exceed limit of time quantum
             if (this.currentTimeSlot >= this.timeQuantum) {
                 this.currentTimeSlot = 0;
@@ -56,4 +59,4 @@ public class CPUScheduler {
         System.out.println("Average turn around time = " + (float) totalTurnaround / (float) nProcess);
         System.out.println("Total burst time = " + totalBurstTime);
     }
-}
+}s
